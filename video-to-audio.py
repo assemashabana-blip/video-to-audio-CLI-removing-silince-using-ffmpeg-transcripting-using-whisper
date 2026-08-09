73b6f7c8095ccc6669151968b68b343bec10b10e
Assem Shabana <assemashabana@gmail.com>
2026-08-09 15:57:22 +0300

    made it look good

--- video-to-audio.py
+++ video-to-audio.py
@@ -2,24 +2,35 @@ import subprocess
 import argparse
 import sys
 from pathlib import Path
-
-parser = argparse.ArgumentParser(description="Convert video to audio.")
-parser.add_argument("video", help="Path to the input video file")
-args = parser.parse_args()
-filename = args.video
-
-
-if not filename.lower().endswith((".mp4", ".mkv", ".avi", ".webm", ".flv", ".ts")):
-    print(f"Unsupported format: {filename}", file=sys.stderr)
-    sys.exit(1)
-
-if not Path(filename).is_file():
-    print(f"File not found: {filename}", file=sys.stderr)
-    sys.exit(1)
+def main():
+    parser = argparse.ArgumentParser(description="Convert video to audio.")
+    parser.add_argument("video", help="Path to the input video file")
+    args = parser.parse_args()
+    file_validation(args.video)
+    output_audio=renaming_the_file(args.video)
+    subprocess.run(["ffmpeg", "-i", args.video, "-vn", "-acodec", "libmp3lame", str(output_audio)])
 
 
-output_audio = Path(filename).with_suffix(".mp3")
-print(output_audio)
+def file_validation(filename):
+    if not filename.lower().endswith((".mp4", ".mkv", ".avi", ".webm", ".flv", ".ts")):
+        print(f"Unsupported format: {filename}", file=sys.stderr)
+        sys.exit(1)
+
+    if not Path(filename).is_file():
+        print(f"File not found: {filename}", file=sys.stderr)
+        sys.exit(1)
+
+
+def renaming_the_file(filename):
+    output_audio = Path(filename).with_suffix(".mp3")
+    return output_audio
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
     
         
